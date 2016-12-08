f5389d85e4a73eea11c9093c8adff53dbfe44f7e
Yannick Marcon <yannick.marcon@obiba.org>
2016-12-08 19:39:31 +0100

    HOME env for pandoc added

--- src/dist/conf/Rprofile.R
+++ src/dist/conf/Rprofile.R
@@ -3,4 +3,6 @@ if (!file.exists("/var/lib/rserver/R/library")) {
   dir.create("/var/lib/rserver/R/library", recursive=TRUE)
 }
 .libPaths("/var/lib/rserver/R/library")
+# for Pandoc
+Sys.setenv(HOME="/var/lib/rserver")
 # newline required
